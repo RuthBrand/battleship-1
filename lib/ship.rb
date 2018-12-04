9120785ebfec4ca8fb2c25e6c4b047c3b65f9d2a
Noah Flint <noah.flint24@gmail.com>
2018-12-03 17:09:12 -0700

    created a health method/attribute

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,12 @@
 class Ship
   attr_reader :name,
-              :length
+              :length,
+              :health
 
   def initialize(name, length)
     @name = name
     @length = length
+    @health = length
   end
 
 
