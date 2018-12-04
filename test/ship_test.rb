e916ffbad8f246fc09043da1553b8b1f520d499d
Noah Flint <noah.flint24@gmail.com>
2018-12-03 17:07:17 -0700

    created a health test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -18,9 +18,17 @@ class ShipTest < Minitest::Test
   def test_it_has_a_length
     ship = Ship.new("Cruiser", 3)
     assert_equal 3, ship.length
-
-
   end
+
+  def test_ship_has_a_health
+    ship = Ship.new("Cruiser", 3)
+    assert_equal 3, ship.health
+  end
+
+
+
+
+
 
 
 
