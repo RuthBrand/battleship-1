fe0299dd7099eb0a42603c9729345648e03d3d47
Noah Flint <noah.flint24@gmail.com>
2018-12-03 17:04:47 -0700

    created a length method

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -6,12 +6,12 @@ require './lib/ship'
 class ShipTest < Minitest::Test
 
   def test_it_exists
-    ship = Ship.new("Cruiser")
+    ship = Ship.new("Cruiser", 3)
     assert_instance_of Ship, ship
   end
 
   def test_it_has_a_name
-    ship = Ship.new("Cruiser")
+    ship = Ship.new("Cruiser", 3)
     assert_equal "Cruiser", ship.name
   end
 
