2c1ce0b63dca5466619c42e820356ef6da6d10b7
Noah Flint <noah.flint24@gmail.com>
2018-12-07 11:42:07 -0700

    created battleship test, and file, created test_it_exists

---
+++ test/battleship_test.rb
@@ -0,0 +1,17 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/battleship'
+
+
+class BattleshipTest < MiniTest::Test
+
+  def test_it_exists
+    battleship = Battleship.new
+
+    assert_instance_of Battleship, battleship
+  end
+end
