6222c42cea68117e8d0af91d68e00e6d711d5952
Tim Allen <tnelsallen3@gmail.com>
2018-12-07 19:03:28 -0700

    added computer randomizer

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -7,7 +7,6 @@ class Battleship
 
   def start
     main_menu
-
   end
 
   def main_menu
@@ -27,46 +26,62 @@ class Battleship
 
   def setup
     puts "\n"
-    board = Board.new
+
+    computer_board = Board.new
     computer_cruiser = Ship.new("Cruiser", 3)
     computer_sub = Ship.new("Submarine", 2)
-    #here goes the code for random but valid computer ship placement
+
+    random_array = computer_board.cells.keys.sample(3)
+    while computer_board.valid_placement?(computer_cruiser, random_array) == false
+      random_array = computer_board.cells.keys.sample(3)
+    end
+    computer_board.place(computer_cruiser, random_array)
+
+    random_array = computer_board.cells.keys.sample(2)
+    while computer_board.valid_placement?(computer_sub, random_array) == false
+      random_array = computer_board.cells.keys.sample(2)
+    end
+    computer_board.place(computer_sub, random_array)
+
+    puts "I have laid out my ships on the grid.\nYou now need to lay out your ships.\nThe Cruiser is three units long and the Submarine is two units long."
+
+    user_board = Board.new
     user_cruiser = Ship.new("Cruiser", 3)
     user_sub = Ship.new("Submarine", 2)
 
-    input = nil
-    puts "I have laid out my ships on the grid.\nYou now need to lay out your ships.\nThe Cruiser is three units long and the Submarine is two units long."
-    puts board.render
+    puts user_board.render
     puts "Enter the squares for the Cruiser (3 spaces):"
     print ">"
+
     input = gets.chomp
     input_array = input.split
-    while board.valid_placement?(user_cruiser, input_array) == false
-
+    while user_board.valid_placement?(user_cruiser, input_array) == false
       puts "Those are invalid coordinates. Please try again:"
       print ">"
       input = gets.chomp
       input_array = input.split
-      # require 'pry'; binding.pry
     end
-    board.place(user_cruiser, input_array)
+    user_board.place(user_cruiser, input_array)
+
     puts "\n"
-    puts board.render(true)
+    puts user_board.render(true)
     puts "Enter the squares for the Submarine (2 spaces):"
     print ">"
+
+    input = gets.chomp
+    input_array = input.split
+    while user_board.valid_placement?(user_cruiser, input_array) == false
+      puts "Those are invalid coordinates. Please try again:"
+      print ">"
+      input = gets.chomp
+      input_array = input.split
+    end
+    user_board.place(user_sub, input_array)
+
+    turn
   end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  def turn
+    main_menu
+  end
 end
